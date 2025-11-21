fa8f6cbb935179b03a17ab31b255bfe85e0ce039
RajeshNagar701 <rajeshnagar.tops@gmail.com>
2025-11-21 17:34:06 +0530

    first commit

---
+++ laravel/demoapp/resources/views/blade_template.blade.php
@@ -0,0 +1,134 @@
+
+<?php
+/*
+
+What is blade template
+
+The Blade is a powerful templating engine in a Laravel framework. 
+The blade allows to use the templating engine easily, and it makes the 
+syntax writing very simple. 
+
+The blade templating engine provides its own structure such as 
+conditional statements and loops. 
+
+To create a blade template, you just need to create a view file and save it with 
+a .blade.php extension instead of .php extension. 
+
+The blade templates are stored in the /resources/view directory. The main advantage 
+of using the blade template is that we can create the master template, 
+which can be extended by other files.
+
+
+
+*/
+?>
+
+
+
+{{- this blade comment -}}
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+		<!-- Latest compiled and minified CSS -->
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
+		<!-- Latest compiled JavaScript -->
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    </head>
+    <body>
+        
+		<h1>Hello Welcome Balde Template</h1>
+		
+		<?php 
+			$name="Raj nagar";
+			echo "<h1>Hi hello " . $name . "</h1>"; 
+		?>
+		
+		<h1>{{"Hi hello " . $name }}</h1>
+    
+		<h1>{{10+10}}</h1>
+		<h1><?php echo 10+10; ?></h1>
+	
+	
+		<?php 
+			$a=10;
+		?>
+		
+		@php
+			$a=10;
+		@endphp
+		
+		<hr>
+		
+		<?php
+		/*
+		Blade Conditional Directives
+		
+		@php @endphp
+		@if  @elseif @else and @endif  
+		@unless @endunless inverse of if  opposite of if 
+		@isset @endisset  
+		*/
+		?>
+		<?php $name="Mahesh"?>
+		@if($name=="Raj")
+		<h1>Hi my name is {{$name}}</h1>
+		@elseif($name=="Mahesh")
+		<h1>Hi my name is {{$name}}</h1>
+		@else
+		<h1>Unknown</h1>	
+		@endif
+
+		<hr>
+		
+		<?php
+		/*
+		Blade Looping Directives
+
+		@for and @endfor
+		@while and @endwhile
+		@foreach and @endforeach
+		@break @continue
+		
+		*/
+		?>
+
+		@for($i=1;$i<=10;$i++)
+		<h4>{{$i}}</h4>	
+		@endfor
+	
+		<?php $data=['sam','raj','mahesh'];?>
+
+		@foreach($data as $d)
+		<h4>{{$d}}</h4>
+		@endforeach
+
+
+	
+		<hr>
+	
+	
+	
+		<?php
+		/*
+		Layout Blade Directives
+		@include
+		@yield directive is used to display the content of a given section
+		@section and @endsection   directives define a section of content
+
+		@extends blade directives specify which layout the child view should “inherit”
+
+		@stack  render the complete stack content , pass the name of the stake
+		@push and @endpush  is used to push the stack
+
+
+		
+		*/
+		?>
+	
+	
+	</body>
+</html>
